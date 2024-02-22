54253c508ee9322be56e59cb81278004d03f4672
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2024-02-22 06:11:07 -0800

    limit the number of search results shown

--- src/context/viewer-context.tsx
+++ src/context/viewer-context.tsx
@@ -19,6 +19,9 @@ export type ViewerConfigOptions = {
   background?: string;
   canvasBackgroundColor?: string;
   canvasHeight?: string;
+  contentSearch?: {
+    searchResultsLimit?: number;
+  };
   ignoreCaptionLabels?: string[];
   informationPanel?: {
     open?: boolean;
@@ -48,6 +51,9 @@ const defaultConfigOptions = {
   background: "transparent",
   canvasBackgroundColor: "#6662",
   canvasHeight: "61.8vh",
+  contentSearch: {
+    searchResultsLimit: 20,
+  },
   ignoreCaptionLabels: [],
   informationPanel: {
     open: true,
