fa170662553803cef35bfc6aee36477b9309472c
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2024-03-05 15:45:51 -0800

    export annotation and helpers

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,21 @@
 import Primitives from "src/components/Primitives";
 import Slider from "src/components/Slider";
 import Viewer from "src/components/Viewer";
+import {
+  parseAnnotationTarget,
+  parseAnnotationsFromAnnotationResources,
+  type AnnotationTargetExtended,
+} from "src/lib/annotation-helpers";
+import { createOpenSeadragonRect } from "src/lib/openseadragon-helpers";
 
-export { Primitives, Slider, Viewer };
+export {
+  Primitives,
+  Slider,
+  Viewer,
+  parseAnnotationTarget,
+  parseAnnotationsFromAnnotationResources,
+  type AnnotationTargetExtended,
+  createOpenSeadragonRect,
+};
 
 export default Viewer;
