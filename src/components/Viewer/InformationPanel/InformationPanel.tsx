007476631bee296c961744ca97c1d7d4fbbbef41
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2024-03-09 19:28:04 -0800

    improve error handling - add console.log, ErrorFallback, try...catch

--- src/components/Viewer/InformationPanel/InformationPanel.tsx
+++ src/components/Viewer/InformationPanel/InformationPanel.tsx
@@ -24,6 +24,9 @@ import {
 } from "@iiif/presentation-3";
 import { Label } from "src/components/Primitives";
 import { setupPlugins, formatPluginAnnotations } from "src/lib/plugin-helpers";
+import ErrorFallback from "../Viewer/ErrorFallback";
+
+import { ErrorBoundary } from "react-error-boundary";
 
 const UserScrollTimeout = 1500; // 1500ms without a user-generated scroll event reverts to auto-scrolling
 
@@ -63,17 +66,21 @@ export const InformationPanel: React.FC<NavigatorProps> = ({
   const renderAnnotation = informationPanel?.renderAnnotation;
   const renderContentSearch = informationPanel?.renderContentSearch;
 
-  const canvas: CanvasNormalized = vault.get({
-    id: activeCanvas,
-    type: "Canvas",
-  });
+  let canvas: CanvasNormalized;
+  try {
+    canvas = vault.get({
+      id: activeCanvas,
+      type: "Canvas",
+    });
+  } catch (error) {
+    console.error(error);
+  }
 
   const { pluginsWithInfoPanel, pluginsAnnotationPageIds } =
     setupPlugins(plugins);
 
   function renderPluginLabel(plugin: PluginConfig, i: number) {
     const annotations = formatPluginAnnotations(plugin, annotationResources);
-
     if (
       annotations.length === 0 &&
       plugin.informationPanel?.displayIfNoAnnotations === false
@@ -82,6 +89,7 @@ export const InformationPanel: React.FC<NavigatorProps> = ({
     }
 
     const label = plugin.informationPanel?.label || { none: [plugin.id] };
+
     return (
       <Trigger key={i} value={plugin.id}>
         <Label label={label} />
@@ -112,16 +120,18 @@ export const InformationPanel: React.FC<NavigatorProps> = ({
 
     return (
       <Content key={i} value={plugin.id}>
-        <PluginInformationPanelComponent
-          annotations={annotations}
-          {...plugin?.informationPanel?.componentProps}
-          activeManifest={activeManifest}
-          canvas={canvas}
-          viewerConfigOptions={configOptions}
-          openSeadragonViewer={openSeadragonViewer}
-          useViewerDispatch={useViewerDispatch}
-          useViewerState={useViewerState}
-        />
+        <ErrorBoundary FallbackComponent={ErrorFallback}>
+          <PluginInformationPanelComponent
+            annotations={annotations}
+            {...plugin?.informationPanel?.componentProps}
+            activeManifest={activeManifest}
+            canvas={canvas}
+            viewerConfigOptions={configOptions}
+            openSeadragonViewer={openSeadragonViewer}
+            useViewerDispatch={useViewerDispatch}
+            useViewerState={useViewerState}
+          />
+        </ErrorBoundary>
       </Content>
     );
   }
--- src/components/Viewer/Viewer/Content.tsx
+++ src/components/Viewer/Viewer/Content.tsx
@@ -18,6 +18,8 @@ import Media from "src/components/Viewer/Media/Media";
 import Painting from "../Painting/Painting";
 import React from "react";
 import { useViewerState } from "src/context/viewer-context";
+import ErrorFallback from "./ErrorFallback";
+import { ErrorBoundary } from "react-error-boundary";
 
 export interface ViewerContentProps {
   activeCanvas: string;
@@ -88,13 +90,15 @@ const ViewerContent: React.FC<ViewerContentProps> = ({
       {(isAside || isForcedAside) && (
         <Aside>
           <CollapsibleContent>
-            <InformationPanel
-              activeCanvas={activeCanvas}
-              annotationResources={annotationResources}
-              searchServiceUrl={searchServiceUrl}
-              setContentSearchResource={setContentSearchResource}
-              contentSearchResource={contentSearchResource}
-            />
+            <ErrorBoundary FallbackComponent={ErrorFallback}>
+              <InformationPanel
+                activeCanvas={activeCanvas}
+                annotationResources={annotationResources}
+                searchServiceUrl={searchServiceUrl}
+                setContentSearchResource={setContentSearchResource}
+                contentSearchResource={contentSearchResource}
+              />
+            </ErrorBoundary>
           </CollapsibleContent>
         </Aside>
       )}
